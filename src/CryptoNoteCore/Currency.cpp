5bf9e161ce7c0c2320994f8d1d7f56b81e824bdd
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-08-12 13:26:54 -0700

    Added an extra l

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -546,7 +546,7 @@ Difficulty Currency::nextDifficulty(uint8_t version, uint32_t blockIndex, std::v
     }
     uint64_t nextDiffZ = low / timeSpan;
     if(height == 5000 || height == 6500 || height == 7000){
-           printf("Low:%lu   timeSpan:%llu     NextDiff:%lu    height:%lu\n",low,timeSpan,nextDiffZ,blockIndex);
+           printf("Low:%lu   timeSpan:%lu     NextDiff:%lu    height:%lu\n",low,timeSpan,nextDiffZ,blockIndex);
 
          }
     return nextDiffZ;

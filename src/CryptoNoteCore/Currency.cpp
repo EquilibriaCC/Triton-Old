b75f844b3a5eb8804523ae5f11337446329d461d
Harrison Hesslink <harrisonhesslink24@gmail.com>
2018-08-21 19:48:41 -0700

    rip t

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -442,7 +442,7 @@ Difficulty Currency::nextDifficulty(uint8_t version, uint32_t blockIndex, std::v
 	if (blockIndex >= (UPGRADE_HEIGHT_V5-1) && blockIndex <= (UPGRADE_HEIGHT_V5+DIFFICULTY_WINDOW_V5) ) {return 10000000;}
 
 	if (version >= BLOCK_MAJOR_VERSION_5) {
-    prinf("YO ITS THIS ALLOY ALGO MUTHAFUCKA");
+    printf("YO ITS THIS ALLOY ALGO MUTHAFUCKA");
 		int64_t T = m_difficultyTarget;
 
     if(blockIndex == 24922){
